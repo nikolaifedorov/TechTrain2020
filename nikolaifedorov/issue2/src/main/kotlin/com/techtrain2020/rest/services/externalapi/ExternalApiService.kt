78f6842f051499e1ab7d9595561671da712ac871
Nikolai Fedorov <nikolas.fedorov@gmail.com>
2020-06-07 19:17:13 +0300

    #2: added processing exceptions for connect to external rest service.

--- nikolaifedorov/issue2/src/main/kotlin/com/techtrain2020/rest/services/externalapi/ExternalApiService.kt
+++ nikolaifedorov/issue2/src/main/kotlin/com/techtrain2020/rest/services/externalapi/ExternalApiService.kt
@@ -1,18 +1,20 @@
 package com.techtrain2020.rest.services.externalapi
 
 import org.springframework.stereotype.Service
+import java.io.IOException
 import java.net.URL
 
 
 @Service
 class ExternalApiService {
 
+    private val externalApiUrl = "http://localhost:8080/"
     private val expectedResponse = "МЫ"
 
-    val url = URL("http://localhost:8080/")
-
     fun get(): String {
-        val response: String = url.readText()
+        val url = try { URL(externalApiUrl) } catch (e: IOException)  { null } ?: return ""
+
+        val response: String = try { url.readText() } catch (e: IOException)  { "" }
 
         if (response == expectedResponse) {
             return response
