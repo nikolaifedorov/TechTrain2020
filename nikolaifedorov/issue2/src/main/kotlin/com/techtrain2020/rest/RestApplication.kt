233f54b95b3cbe63075f941dd9eba01796ba14eb
Nikolai Fedorov <nikolas.fedorov@gmail.com>
2020-06-07 17:11:02 +0300

    #2: init Kotlin + Spring Boot project

---
+++ nikolaifedorov/issue2/src/main/kotlin/com/techtrain2020/rest/RestApplication.kt
@@ -0,0 +1,11 @@
+package com.techtrain2020.rest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestApplication>(*args)
+}
